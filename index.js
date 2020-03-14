ced6dee34392189ce1b09271121aa5829b39bb4e
thecodrr <thecodrr@protonmail.com>
2020-03-14 18:04:19 +0500

    fix: some inconsistencies between sync & async apis

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function sync(dir, options) {
   if (options.includeDirs) paths.push(dir);
   if (--options.maxDepth < 0) return paths;
 
-  dirents.forEach(async dirent => {
-    const res = `${dir}${path.sep}${dirent.name}`;
+  dirents.forEach(dirent => {
+    let res = `${dir}${path.sep}${dirent.name}`;
     if (dirent.isDirectory()) {
       if (options.excludedDirs && options.excludedDirs[dirent.name]) return;
       sync(res, options).forEach(push.bind(paths));
@@ -32,12 +32,13 @@ async function async(dir, options) {
   const paths = [];
 
   if (options.includeDirs) paths.push(dir);
-  if (--options.maxDepth < 0) resolve(paths);
+  if (--options.maxDepth < 0) return paths;
 
   await Promise.all(
     dirents.map(async dirent => {
-      const res = `${dir}${path.sep}${dirent.name}`;
+      let res = `${dir}${path.sep}${dirent.name}`;
       if (dirent.isDirectory()) {
+        if (options.excludedDirs && options.excludedDirs[dirent.name]) return;
         (await async(res, options)).forEach(push.bind(paths));
       } else {
         if (!options.includeBasePath) res = dirent.name;
@@ -48,7 +49,7 @@ async function async(dir, options) {
   return paths;
 }
 
-function fdir(options) {
+function getOptions(options) {
   const defaultOptions = {
     includeDirs: false,
     includeBasePath: true,
@@ -57,17 +58,18 @@ function fdir(options) {
     resolvePaths: false,
     excludedDirs: undefined
   };
-  options = !options ? defaultOptions : { ...defaultOptions, ...options };
-  return {
-    sync: dir => {
-      if (options.resolvePaths) dir = path.resolve(dir);
-      return sync(dir, options);
-    },
-    async: dir => {
-      if (options.resolvePaths) dir = path.resolve(dir);
-      return async(dir, options);
-    }
-  };
+  return !options ? defaultOptions : { ...defaultOptions, ...options };
 }
 
-module.exports = fdir;
+module.exports = {
+  sync: (dir, options) => {
+    options = getOptions(options);
+    if (options.resolvePaths) dir = path.resolve(dir);
+    return sync(dir, options);
+  },
+  async: (dir, options) => {
+    options = getOptions(options);
+    if (options.resolvePaths) dir = path.resolve(dir);
+    return async(dir, options);
+  }
+};
