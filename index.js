aca9f795482bec0bf634dd9744af901dcf3767be
thecodrr <thecodrr@protonmail.com>
2020-03-21 13:38:39 +0500

    fix: handle errors properly (#8)

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sync(dir, options = {}) {
 }
 
 function async(dir, options = {}) {
-  return new Promise(function(pResolve) {
+  return new Promise(function(pResolve, pReject) {
     const paths = [];
     if (options.resolvePaths) dir = resolve(dir);
     const dirs = [dir];
@@ -35,7 +35,8 @@ function async(dir, options = {}) {
         }
         const dir = dirs[cursor];
         if (options.includeDirs) paths[paths.length] = dir;
-        fs.readdir(dir, readdirOpts, function(_, dirents) {
+        fs.readdir(dir, readdirOpts, function(err, dirents) {
+          if (err) return pReject(getError(err, dir));
           for (var j = 0; j < dirents.length; ++j) {
             recurse(dirents[j], dir, paths, options, dirs);
           }
@@ -73,6 +74,25 @@ function recurse(dirent, dir, paths, options, dirs) {
   if (options.excludeBasePath) fullPath = dirName;
   if (!options.searchFn || options.searchFn(fullPath))
     paths[paths.length] = fullPath;
+}
+
+function getError(error, path) {
+  let message = "";
+  switch (error.code) {
+    case "EACCES":
+      message = `You do not have permission to access "${path}".`;
+      break;
+    case "ENOENT":
+      message = `"${path}" does not exist or an invalid path was provided. Are you sure this is a valid directory?`;
+      break;
+    case "ENOTDIR":
+      message = `"${path}" is not a directory as expected. fdir only works with directories.`;
+      break;
+  }
+  return {
+    ...error,
+    message: `${message}\nIf you think this is a bug, please open an issue at: https://github.com/thecodrr/fdir`
+  };
 }
 
 module.exports = {
