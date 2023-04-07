75c0cff470ff1c8f62919cee115920c3da9966ea
Abdullah Atta <abdullahatta@streetwriters.co>
2023-04-07 06:21:36 +0500

    test: generate `lcov` coverage report

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: "c8",
+      reporter: ["lcov", "text"],
+    },
+  },
+});
