43360569a9dc52b0f5c0245a5e6d279e0af19855
Abdullah Atta <abdullahatta@streetwriters.co>
2023-08-01 01:31:25 +0500

    chore: update glob benchmark and libraries

--- benchmarks/glob-benchmark.ts
+++ benchmarks/glob-benchmark.ts
@@ -1,5 +1,5 @@
 import { fdir } from "../index";
-import glob from "glob";
+import { glob, globSync } from "glob";
 import fg from "fast-glob";
 import tg from "tiny-glob";
 import tgSync from "tiny-glob/sync";
@@ -7,30 +7,21 @@ import b from "benny";
 import packageJson from "../package.json";
 
 async function benchmark() {
-  const counts = new fdir()
-    .glob("**.js")
-    .onlyCounts()
-    .crawl(".")
-    .sync();
+  const counts = new fdir().glob("**/*.js").onlyCounts().crawl(".").sync();
 
   await b.suite(
-    `Asynchronous (${counts.files} files, ${counts.dirs} folders)`,
+    `Asynchronous (${counts.files} files, ${counts.directories} folders)`,
     b.add(`fdir ${packageJson.version} async`, async () => {
-      await new fdir()
-        .glob("**.js")
-        .crawl(".")
-        .withPromise();
+      await new fdir().glob("**/*.js").crawl(".").withPromise();
     }),
     b.add("glob async", async () => {
-      await new Promise((resolve) => {
-        glob("**/**.js", { dot: true }, () => resolve(undefined));
-      });
+      await glob("**/*.js", { dot: true });
     }),
     b.add("fast-glob async", async () => {
-      await fg("**.js", { dot: true, onlyFiles: true });
+      await fg("**/*.js", { dot: true, onlyFiles: true });
     }),
     b.add("tiny-glob async", async () => {
-      await tg("**/**.js", { dot: true, filesOnly: true });
+      await tg("**/*.js", { dot: true, filesOnly: true });
     }),
     b.cycle(),
     b.complete()
@@ -39,13 +30,10 @@ async function benchmark() {
   await b.suite(
     `Synchronous (${counts.files} files, ${counts.dirs} folders)`,
     b.add(`fdir ${packageJson.version} sync`, () => {
-      new fdir()
-        .glob("**.js")
-        .crawl(".")
-        .sync();
+      new fdir().glob("**.js").crawl(".").sync();
     }),
     b.add("glob sync", () => {
-      glob.sync("**/**.js", { dot: true });
+      globSync("**/**.js", { dot: true });
     }),
     b.add("fast-glob sync", () => {
       fg.sync("**.js", { dot: true });
