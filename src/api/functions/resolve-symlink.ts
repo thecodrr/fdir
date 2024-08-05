e6ebb4410be9121d1cb1962159822c1f07ac30a5
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2024-08-05 15:22:00 +0000

    chore: use `lstat` instead

--- src/api/functions/resolve-symlink.ts
+++ src/api/functions/resolve-symlink.ts
@@ -18,7 +18,7 @@ const resolveSymlinksAsync: ResolveSymlinkFunction = function(
   } = state;
   queue.enqueue();
 
-  fs.stat(path, (error, stat) => {
+  fs.lstat(path, (error, stat) => {
     if (error) {
       queue.dequeue(suppressErrors ? null : error, state);
       return;
@@ -60,7 +60,7 @@ const resolveSymlinksSync: ResolveSymlinkFunction = function(
   callback
 ) {
   try {
-    const stat = fs.statSync(path);
+    const stat = fs.lstatSync(path);
     callback(stat, path);
   } catch (e) {
     if (!state.options.suppressErrors) throw e;
