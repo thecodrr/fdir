903f4cd98ba4fde8345b01e17cf4e23dda4a32db
Abdullah Atta <abdullahatta@streetwriters.co>
2024-10-15 02:58:52 +0500

    fix: simplify recursion detection algorithm

--- src/api/functions/resolve-symlink.ts
+++ src/api/functions/resolve-symlink.ts
@@ -1,7 +1,6 @@
 import fs from "fs";
 import { WalkerState, Options } from "../../types";
 import { dirname } from "path";
-import { readlink } from "fs/promises";
 
 export type ResolveSymlinkFunction = (
   path: string,
@@ -26,16 +25,11 @@ const resolveSymlinksAsync: ResolveSymlinkFunction = function (
     fs.stat(resolvedPath, (error, stat) => {
       if (error) return queue.dequeue(suppressErrors ? null : error, state);
 
-      if (stat.isDirectory()) {
-        isRecursiveAsync(path, resolvedPath, state).then((isRecursive) => {
-          if (isRecursive) return queue.dequeue(null, state);
-          callback(stat, useRealPaths ? resolvedPath : path);
-          queue.dequeue(null, state);
-        });
-      } else {
-        callback(stat, useRealPaths ? resolvedPath : path);
-        queue.dequeue(null, state);
-      }
+      if (stat.isDirectory() && isRecursive(path, resolvedPath, state))
+        return queue.dequeue(null, state);
+
+      callback(stat, useRealPaths ? resolvedPath : path);
+      queue.dequeue(null, state);
     });
   });
 };
@@ -72,46 +66,26 @@ export function build(
   return isSynchronous ? resolveSymlinks : resolveSymlinksAsync;
 }
 
-async function isRecursiveAsync(
-  path: string,
-  resolved: string,
-  state: WalkerState
-) {
-  if (state.options.useRealPaths)
-    return isRecursiveUsingRealPaths(resolved, state);
-
-  let parent = dirname(path);
-  if (parent + state.options.pathSeparator === state.root || parent === path)
-    return false;
-  try {
-    const resolvedParent =
-      state.symlinks.get(parent) || (await readlink(parent));
-    if (resolvedParent !== resolved) return false;
-    state.symlinks.set(path, resolved);
-    return true;
-  } catch (e) {
-    return isRecursiveAsync(parent, resolved, state);
-  }
-}
-
-function isRecursiveUsingRealPaths(resolved: string, state: WalkerState) {
-  return state.visited.includes(resolved + state.options.pathSeparator);
-}
-
 function isRecursive(path: string, resolved: string, state: WalkerState) {
   if (state.options.useRealPaths)
     return isRecursiveUsingRealPaths(resolved, state);
 
   let parent = dirname(path);
-  if (parent + state.options.pathSeparator === state.root || parent === path)
-    return false;
-  try {
-    const resolvedParent =
-      state.symlinks.get(parent) || fs.readlinkSync(parent);
-    if (resolvedParent !== resolved) return false;
-    state.symlinks.set(path, resolved);
-    return true;
-  } catch (e) {
-    return isRecursive(parent, resolved, state);
+  let depth = 1;
+  while (parent !== state.root && depth < 2) {
+    const resolvedPath = state.symlinks.get(parent);
+    const isSameRoot =
+      !!resolvedPath &&
+      (resolvedPath === resolved ||
+        resolvedPath.startsWith(resolved) ||
+        resolved.startsWith(resolvedPath));
+    if (isSameRoot) depth++;
+    else parent = dirname(parent);
   }
+  state.symlinks.set(path, resolved);
+  return depth > 1;
+}
+
+function isRecursiveUsingRealPaths(resolved: string, state: WalkerState) {
+  return state.visited.includes(resolved + state.options.pathSeparator);
 }
