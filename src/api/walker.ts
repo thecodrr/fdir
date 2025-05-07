575d497df77e66543126195f5ce78f1e942cc7ad
James Garbutt <43081j@users.noreply.github.com>
2025-05-07 10:40:13 +0100

    chore: run format

--- src/api/walker.ts
+++ src/api/walker.ts
@@ -49,7 +49,7 @@ export class Walker<TOutput extends Output> {
       ),
       symlinks: new Map(),
       visited: [""].slice(0, 0),
-      fs: options.fs ?? nativeFs
+      fs: options.fs ?? nativeFs,
     };
 
     /*
