f18fa9e031413979e846ce137fb45955d27a422f
Alexander Lichter <github@lichter.io>
2025-05-29 07:12:34 +0200

    chore: use node12 target
    Co-authored-by: Superchupu <53496941+SuperchupuDev@users.noreply.github.com>

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'tsdown/config'
 export default defineConfig({
   entry: 'src/index.ts',
   format: ['cjs', 'esm'],
+  target: 'node12',
+  removeNodeProtocol: true,
   dts: true,
   exports: true,
   publint: true
