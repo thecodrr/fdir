a8bf947eed2c87d75ccceca44a080ab87226c2c7
James Garbutt <43081j@users.noreply.github.com>
2025-07-02 12:42:46 +0100

    wip: use a queue
    Of course, we need a queue! Now things work because we're trying to
    async iterate a synchronous task within an asynchronous task.

--- src/api/iterator.ts
+++ src/api/iterator.ts
@@ -2,14 +2,12 @@ import { Options, IterableOutput } from "../types";
 import { Walker } from "./walker";
 
 class WalkerIterator<TOutput extends IterableOutput> {
-  #next: Promise<TOutput | null>;
-  #resolver: (value: TOutput[number] | null) => void;
+  #resolver?: (result: TOutput[number]) => void;
   #walker: Walker<TOutput>;
   #currentGroup?: string[];
+  #queue: TOutput[number][] = [];
 
   public constructor(root: string, options: Options) {
-    this.#resolver = () => {};
-    this.#next = this.#createNext();
     const pushPath = options.group ? this.#pushPath : this.#pushResult;
     this.#walker = new Walker<TOutput>(
       root,
@@ -28,48 +26,37 @@ class WalkerIterator<TOutput extends IterableOutput> {
   };
 
   #pushResult = async (result: TOutput[number]) => {
-    this.#resolver(result);
-    this.#next = this.#createNext(this.#next);
+    this.#queue.push(result);
+    if (this.#resolver) {
+      const resolver = this.#resolver;
+      this.#resolver = undefined;
+      resolver(result);
+    }
   };
 
   #onComplete = () => {
     this.#currentGroup = undefined;
-    this.#resolver(null);
+    this.#complete = true;
   };
 
-  async #createNext(prev?: Promise<TOutput | null>): Promise<TOutput | null> {
-    const next = new Promise<TOutput[number] | null>((resolve) => {
-      this.#resolver = resolve;
-    });
-    if (prev) {
-      const prevResult = await prev;
-      const nextResult = await next;
-      if (prevResult === null || nextResult === null) {
-        return null;
-      }
-      return [...prevResult, nextResult] as TOutput | null;
-    } else {
-      const nextResult = await next;
-      return nextResult === null ? nextResult : ([nextResult] as TOutput);
-    }
-  }
-
   async *[Symbol.asyncIterator]() {
-    let promise = this.#next;
     this.#walker.start();
 
-    let finished = false;
+    while (true) {
+      yield* this.#queue;
+      this.#queue = [];
 
-    while (!finished) {
-      const result = await promise;
-      promise = this.#next;
-      if (result === null) {
-        finished = true;
-      } else {
-        yield* result;
+      if (this.#complete) {
+        return;
       }
+
+      await new Promise<TOutput[number]>((resolve) => {
+        this.#resolver = resolve;
+      });
     }
   }
+
+  #complete: boolean = false;
 }
 
 export function iterator<TOutput extends IterableOutput>(
