89d4b6ae67571e9d47a2a52650fea7c5892e597e
James Garbutt <43081j@users.noreply.github.com>
2025-07-13 14:06:52 +0100

    chore: use typed output

--- src/api/iterator.ts
+++ src/api/iterator.ts
@@ -1,10 +1,10 @@
-import { Options, IterableOutput } from "../types";
+import { Options, IterableOutput, OutputIterator } from "../types";
 import { IteratorWalker } from "./iterator-walker";
 
 export function iterator<TOutput extends IterableOutput>(
   root: string,
   options: Options
-): AsyncIterable<TOutput[number]> {
+): OutputIterator<TOutput> {
   const walker = new IteratorWalker<TOutput>(root, options);
   return walker.start();
 }
