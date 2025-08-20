63225357adb344472215785dfaeddc621686b09c
Jason Dent <jason@streetsidesoftware.nl>
2025-08-20 16:09:59 +0200

    feat: Support fdir builder without picomatch
    Related to #163

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'tsdown/config'
+import { defineConfig } from "tsdown/config";
 
-export default defineConfig({
-  entry: 'src/index.ts',
-  format: ['cjs', 'esm'],
-  target: 'node12',
-  removeNodeProtocol: true,
-  dts: true,
-  exports: true,
-  fixedExtension: true
-})
+export default defineConfig([
+  {
+    entry: "src/index.ts",
+    format: ["cjs", "esm"],
+    target: "node12",
+    removeNodeProtocol: true,
+    dts: true,
+    exports: true,
+    fixedExtension: true,
+  },
+  {
+    entry: "src/builder/builder.ts",
+    format: ["cjs", "esm"],
+    target: "node12",
+    removeNodeProtocol: true,
+    dts: true,
+    exports: true,
+    fixedExtension: true,
+  },
+])
