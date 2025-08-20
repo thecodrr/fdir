8525f035c890ccbed194780dfb3c119ada8bd320
Jason Dent <jason@streetsidesoftware.nl>
2025-08-20 16:50:03 +0200

    Update tsdown.config.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tsdown/config'
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/builder.ts"],
-  format: ["cjs", "esm"],
+  entry: ['src/index.ts', 'src/builder.ts'],
+  format: ['cjs', 'esm'],
   target: "node12",
   removeNodeProtocol: true,
   dts: true,
